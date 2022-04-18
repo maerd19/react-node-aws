8289107a37af4a7d830f766c8d904dbc17c89fd6
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-04-18 08:26:09 -0500

    Head section

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,32 @@
+import Head from 'next/head';
+
 const Layout = ({ children }) => {
+    const head = () => (
+        <link 
+            rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+            crossorigin="anonymous"
+        />
+    );
+
     const nav = () => (
-        <ul className="nav">
+        <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
-                <a className="nav-link">Home</a>
+                <a className="nav-link text-dark" href="">Home</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">Login</a>
+                <a className="nav-link text-dark" href="">Login</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link">Register</a>
+                <a className="nav-link text-dark" href="">Register</a>
             </li>
         </ul>
     )
 
-    return <>
-        {nav()} {children}
-    </>
+    return (<>
+        {head()} {nav()} <div className="container pt-5 pb-5">{children}</div>
+    </>)
 }
 
 export default Layout;
