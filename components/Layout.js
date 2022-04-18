757ac6afb568750e028a7101c7629b13effb8ccb
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-04-18 08:46:41 -0500

    nav progress bar

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,41 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangestart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Layout = ({ children }) => {
     const head = () => (
-        <link 
-            rel="stylesheet" 
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
-            crossorigin="anonymous"
-        />
+        <>
+            <link 
+                rel="stylesheet" 
+                href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+                crossorigin="anonymous"
+            />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js" />
+        </>
     );
 
     const nav = () => (
         <ul className="nav nav-tabs bg-warning">
             <li className="nav-item">
-                <a className="nav-link text-dark" href="">Home</a>
+                <Link href="/">
+                    <a className="nav-link text-dark">Home</a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-dark" href="">Login</a>
+                <Link href="/login">
+                    <a className="nav-link text-dark">Login</a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-dark" href="">Register</a>
+            <Link href="/register">
+                    <a className="nav-link text-dark">Register</a>
+                </Link>
             </li>
         </ul>
     )
