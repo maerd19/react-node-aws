5a9d762290014f620e215bd9591fd55565f11983
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-04-19 04:28:13 -0500

    Loading css

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 Router.onRouteChangestart = url => NProgress.start();
 Router.onRouteChangeComplete = url => NProgress.done();
@@ -16,7 +17,8 @@ const Layout = ({ children }) => {
                 integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
                 crossorigin="anonymous"
             />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js" />
+
+            <link rel="stylesheet" href="/static/css/styles.css" />
         </>
     );
 
